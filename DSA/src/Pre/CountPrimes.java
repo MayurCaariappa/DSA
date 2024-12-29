caa52d8acb8af734b0eba05e013f3829855ca1e0
dheerajupadh <68938734+dheerajupadh@users.noreply.github.com>
2024-12-29 16:10:29 +0530

    Update CountPrimes.java
    Optimal

--- DSA/src/Pre/CountPrimes.java
+++ DSA/src/Pre/CountPrimes.java
@@ -16,12 +16,20 @@ public class CountPrimes {
         return count;
     }
 
-    public static boolean isPrime(int n) {
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                return false;
+        public boolean isPrime(int num){
+        int count =0;
+        for(int i=1;i*i<=num;i++){
+            if(num%i==0){
+                if(i*i == num){
+                    count++;
+                }else{
+                    count += 2;
+                }
             }
         }
-        return true;
+        if(count ==2){
+            return true;
+        }
+        return false;
     }
 }
